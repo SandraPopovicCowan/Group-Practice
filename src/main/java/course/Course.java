e4c67b911c1f6518556124cfc512e1e7c602a840
User <macbookair@Bellas-MacBook-Air.local>
2021-03-22 14:58:14 +0100

    Class Course

--- src/main/java/course/Course.java
+++ src/main/java/course/Course.java
@@ -5,10 +5,54 @@
  */
 package course;
 
-/**
- *
- * @author macbookair
- */
+
 public class Course {
+    private String name;
+    private int numberOfClasses;
+    private String codeName;
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public void setName(String Name) {
+        this.name = Name;
+        
+    }
+    
+    public int getNumberOfClasses() {
+        return this.numberOfClasses;
+    }
+    
+    public void setNumberOfClasses(int NumberOfClasses) {
+        this.numberOfClasses = NumberOfClasses;
+    }
+    
+    public String getCodeName() {
+        return this.codeName;
+    }
+    
+    public void setCodeName(String CodeName) {
+        this.codeName = CodeName;
+    }
+    
+    public Course() {
+        this.name = "";
+        this.codeName = "";
+        this.numberOfClasses = 1;
+    }
+    
+    public Course (String name, String codeName, int numberOfClasses){
+        this.name = name;
+        this.codeName = codeName;
+        this.numberOfClasses = numberOfClasses;
+   
+    }
+    
+    
+    
+    
+    
+    
     
 }
--- src/main/java/student/Student.java
+++ src/main/java/student/Student.java
@@ -5,10 +5,53 @@
  */
 package student;
 
-/**
- *
- * @author macbookair
- */
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    private int yearOfBirth;
     
+     public String getFirstname () {
+         return this.firstName;
+     }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    public String getLastName() {
+        return this.lastName;
+    }
+    
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    
+    public int getYearOfBirht() {
+        return this.yearOfBirth;
+    }
+    
+    public void setYearOfBirth( int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+    
+    public Student() {
+        this.firstName = "";
+        this.lastName = "";
+        this.yearOfBirth = 1990;
+        
+    }
+    
+    public Student( String firstName, String lastName, int yearOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.yearOfBirth = yearOfBirth;
+    }
+    
+    public void info() {
+        System.out.println("First name: " + this.firstName);
+        System.out.println("Last name: " + this.lastName);
+        System.out.println("Year of birth: " + this.yearOfBirth);
+        System.out.println("");
+    }
 }
