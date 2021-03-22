25eaf78d5ce6a088680bb505dfef365502939938
User <macbookair@Bellas-MacBook-Air.local>
2021-03-22 15:47:27 +0100

    Slozeni atribut

--- src/main/java/student/Student.java
+++ src/main/java/student/Student.java
@@ -1,9 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package student;
+import course.Course;
+import computer.Computer;
+
 
 
 public class Student {
